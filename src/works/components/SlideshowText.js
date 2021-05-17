654611c2690dd1647004bdef915f0a94db8332d1
JAMES GLASS <james.glass1155@gmail.com>
2021-05-17 11:09:18 +0900

    eslint err to warn

--- src/works/components/SlideshowText.js
+++ src/works/components/SlideshowText.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import IconButton from '@material-ui/core/IconButton';
